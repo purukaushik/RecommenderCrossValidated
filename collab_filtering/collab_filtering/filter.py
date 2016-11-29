553f808a2f994e15ba62b16927fd88561ef40f75
purukaushik <purukaushik@asu.edu>
2016-11-29 14:59:04 -0700

    Finishing up

--- collab_filtering/collab_filtering/filter.py
+++ collab_filtering/collab_filtering/filter.py
@@ -1,14 +1,16 @@
-# --- Import Libraries --- #
+#!/usr/env/python
+import json
 
 import pandas as pd
 from pymongo import MongoClient, errors
 from scipy.spatial.distance import cosine
-import json
 
 DB = 'dvproject'
 POSTS = 'dbposts0'
 HOST = 'ec2-52-43-158-164.us-west-2.compute.amazonaws.com'
 RELATED_POSTS = 'dbrelposts0'
+COSINE_POSTS = 'dbrelcosineposts0'
+
 
 def debug(type_):
     print "DEBUG: " + str(type_)
@@ -24,7 +26,7 @@ def getStuffFromRecord(record):
     if creator_id:
         creator = int(creator_id)
         answerers.append(creator)
-    return topics, answerers
+    return topics, answerers, question
 
 
 def getFromDB():
@@ -42,6 +44,44 @@ def getFromDB():
 def getScore(history, similarities):
     return sum(history * similarities) / sum(similarities)
 
+
+def collab_filter(topic_map):
+    """
+    Performs collab filtering on dict containing either users<->Topics or questions<->Topics
+
+    :return: a JSON containing the collab filtering matrix metrics
+    for each topic nad related topics
+    """
+    topic_data = pd.DataFrame.from_dict(topic_map, orient='index').fillna(0)
+    data = pd.read_csv('data.csv')
+    data_ibs = pd.DataFrame(index=topic_data.columns, columns=topic_data.columns)
+    for i in range(0, len(data_ibs.columns)):
+        # Loop through the columns for each column
+        for j in range(0, len(data_ibs.columns)):
+            # Fill in placeholder with cosine similarities
+            data_ibs.ix[i, j] = 1 - cosine(topic_data.ix[:, i], topic_data.ix[:, j])
+    json_ = json.loads(data_ibs.to_json())
+    return json_
+
+
+def mongo_write_json(json_result, collectionName):
+    mongo_ = []
+    debug("Building json for mongodb on " + collectionName)
+    for k, v in json_result.iteritems():
+        related_topics = [{"name": str(k_), "value": v_} for k_, v_ in v.iteritems()]
+        mongo_.append({"name": str(k), "related_topics": related_topics})
+    debug("Connecting to mongo on " + HOST + ":" + "27017")
+    try:
+        conn = MongoClient(HOST, 27017)
+        collection = conn[DB][collectionName]
+        debug("Dropping stuff in collection " + collectionName)
+        collection.remove({})
+        debug("Inserting prepared stuff in collection " + collectionName)
+        collection.insert(mongo_)
+    except errors.PyMongoError as error:
+        print "ERROR!: " + error.message
+
+
 if __name__ == "__main__":
 
     g = getFromDB()  # g::Cursor
@@ -49,49 +89,34 @@ if __name__ == "__main__":
         print "Nothing in DB to process or worse(! :() error!. Exiting..."
         exit()
 
-    datadict = dict()
-    topicslist = set()
+    userTopicMap = dict()
+    topicsList = set()
+    questionTopicMap = dict()
 
-    for topics, answerers in g:
+    for topics, answerers, question in g:
         topic_set = set(topics)
-        topicslist.union(topic_set)
-        # print topics, answerers
+        topicsList.union(topic_set)
+
         col = {}
+        col_ = {}
+        for topic in topics:
+            col_[topic] = 1
+            questionTopicMap[question] = col_
+
         for answerer in answerers:
-            # print answerer
-            if answerer not in datadict:
-                # print "not in"
+
+            if answerer not in userTopicMap:
+
                 col = dict()
                 for topic in topics:
                     col[topic] = 1
             else:
-                col = datadict.get(answerer)
+                col = userTopicMap.get(answerer)
                 for topic in topics:
                     col[topic] = col.get(topic, 0) + 1
-            datadict[answerer] = col
+            userTopicMap[answerer] = col
+    debug("Running collab filtering on userTopicMap")
+    mongo_write_json(collab_filter(userTopicMap), collectionName=RELATED_POSTS)
 
-    topic_data = pd.DataFrame.from_dict(datadict, orient='index').fillna(0)
-
-    data = pd.read_csv('data.csv')
-
-    data_ibs = pd.DataFrame(index=topic_data.columns, columns=topic_data.columns)
-
-    for i in range(0, len(data_ibs.columns)):
-        # Loop through the columns for each column
-        for j in range(0, len(data_ibs.columns)):
-            # Fill in placeholder with cosine similarities
-            data_ibs.ix[i, j] = 1 - cosine(topic_data.ix[:, i], topic_data.ix[:, j])
-    json_ = json.loads(data_ibs.to_json())
-    mongo_ = []
-
-    for k,v in json_.iteritems():
-        related_topics = [{"name": str(k_), "value": v_} for k_, v_ in v.iteritems()]
-
-        mongo_.append({"name": str(k), "related_topics": related_topics})
-
-    try:
-        conn = MongoClient(HOST, 27017)
-        collection = conn[DB][RELATED_POSTS]
-        collection.insert(mongo_)
-    except errors.PyMongoError as error:
-        print "ERROR!: " + error.message
+    debug("Running collab filtering on questionTopicMap")
+    mongo_write_json(collab_filter(questionTopicMap), collectionName=COSINE_POSTS)
