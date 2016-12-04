1aceb8296b49f1f431d75cc894805d9a2c37e971
purukaushik <purukaushik@asu.edu>
2016-12-03 22:37:38 -0700

    recoverable restarts

--- flask_app/app/core_service.py
+++ flask_app/app/core_service.py
@@ -11,7 +11,7 @@ PAGE_NO = 'pageNo'
 COLLAB_FILTER = 'dbrelposts0'
 COSINE_SIM = 'dbrelcosineposts0'
 TOPICS = 'dbtopics1'
-PERCENTILES = ''
+PERCENTILES = 'dbmetadata0'
 AGGREGATE = 'dbaggregate0'
 # Flask INIT
 app = Flask(__name__)
@@ -20,6 +20,17 @@ CORS(app)
 
 # MONGODB Connection
 conn = MongoClient(host=app.config['MONGODB_HOST'], port=app.config['MONGODB_PORT'])
+# question score %iles
+qsp = []
+# support score %iles
+ssp = []
+# average view count %iles
+avp = []
+MAPPING = {
+    0: "Low",
+    1: "Medium",
+    2: "High"
+}
 
 
 def get(algo, topic, list_topics=False):
@@ -35,15 +46,14 @@ def get(algo, topic, list_topics=False):
     if list_topics:
         related_topics_list = [x.get('name') for x in related_topics[0]]
 
-        return {"topic": topic,
-                "related_topics_list": related_topics_list
+        return {"topics": related_topics_list
                 }
     return related_topics[0]
 
 
-def getDescription(topic):    
+def getDescription(topic):
     cursor = conn[DB][TOPICS].find({"name": unicode(topic)}, {"desc": 1})
-    if cursor.count:
+    if cursor.count():
         description = str(cursor[0].get("desc"))
     else:
         description = {}
@@ -51,9 +61,14 @@ def getDescription(topic):
 
 
 def get_topics():
-    db_collection = conn[DB][TOPICS]
+    # TODO : return list of topics from dbtopics table instead of collabf table
+    # db_collection = conn[DB][TOPICS]
+    # cursor = db_collection.find()
+    # topics = [topic.get("name") for topic in cursor]
+    db_collection = conn[DB][COLLAB_FILTER]
     cursor = db_collection.find()
-    topics = [topic.get("name") for topic in cursor]
+    first = cursor[0]
+    topics = [x.get("name") for x in first.get("related_topics")]
     return {"topics": topics}
 
 
@@ -63,28 +78,34 @@ def filter_reco(topic, collabCount, cosineCount, averageViews, averageUpvote, su
     cursor = conn[DB][COLLAB_FILTER].find({"name": topic}, {"related_topics": 1})
     collabvalues = sorted(cursor[0].get('related_topics'), key=lambda x: x.get('value'), reverse=True)
 
-    # .. ii. For each apply averageViews filter ->
-    # ....a. obtain %le from other table,
-    cursor = conn[DB][PERCENTILES].find({"name": topic})
-    minViewPerc, maxViewPerc = map(int, cursor[0].get(averageViews).split("-"))
-    minUpVotePerc, maxUpVotePerc = map(int, cursor[0].get(averageUpvote).split("-"))
-    minSupportPerc, maxSupportPerc = map(int, cursor[0].get(support).split("-"))
+    cursor = conn[DB][COSINE_SIM].find({"name": topic}, {"related_topics": 1})
+    cosineValues = sorted(cursor[0].get('related_topics'), key=lambda x: x.get('value'), reverse=True)
+
+    collabvalues = filterByFilter(averageUpvote, averageViews, collabvalues, support)
+    cosineValues = filterByFilter(averageUpvote, averageViews, cosineValues, support)
+
+    return {"collab" : collabvalues, "cosine" : cosineValues}
 
 
-    # ....b. get values for this topic
-    cursor = conn[DB][AGGREGATE].find({"name": topic})
-    avgViewCount = cursor[0].get("Avg_View_count")
-    averageUpvoteCount = cursor[0].get("Avg_Question_score")
-    supportCount = cursor[0].get("Support_Percentile")
+def filterByFilter(averageUpvote, averageViews, collabvalues, support):
 
-    # ....c. check %le with averageViews
+    AVP__ = unicode(MAPPING.get(int(averageViews)))
+    QSP__ = unicode(MAPPING.get(int(averageUpvote)))
+    SSP__ = unicode(MAPPING.get(int(support)))
 
-    # ....d. filter if averageViews in curr topic %le
-    # ...iii. Do the same for averageUpvote
+    avLow, avHigh = map(float, avp[0].get(AVP__).split("-"))
+    avuLow, avuHigh = map(float, qsp[0].get(QSP__).split("-"))
+    sspLow, sspHigh = map(float, ssp[0].get(SSP__).split("-"))
 
-    # 2. COSINE -> do same steps as above
+    def genFilterFn(relTopic, quirk, low, high):
+        cursor = conn[DB][AGGREGATE].find({"Topic": relTopic})
+        k__ = cursor[0].get(quirk)
+        return low <= k__ and k__ <= high
 
-    return {}
+    collabvalues = filter(lambda x: genFilterFn(x.get("name"), "Avg_Question_score", avuLow, avuHigh), collabvalues)
+    collabvalues = filter(lambda x: genFilterFn(x.get("name"), "Avg_View_count", avLow, avHigh), collabvalues)
+    collabvalues = filter(lambda x: genFilterFn(x.get("name"), "Support_Percentile", sspLow, sspHigh), collabvalues)
+    return collabvalues
 
 
 @app.route("/", strict_slashes=False)
@@ -104,16 +125,6 @@ def list_topics():
     return jsonify(get_topics())
 
 
-# NOT IN USE
-def deprecated(dummy):
-    """
-    Function is deprecated
-    :return:
-    """
-    yield NotImplementedError
-
-
-@deprecated
 @app.route("/<algo>", methods=["GET"], strict_slashes=False)
 def get_filter_data(algo):
     if len(request.args) != 0:
@@ -139,7 +150,6 @@ def get_related_data():
             })
 
 
-
 @app.route("/recommendation", methods=["GET"], strict_slashes=False)
 def get_recommendation_data():
     if len(request.args):
@@ -149,6 +159,8 @@ def get_recommendation_data():
         averageUpvote = request.args.get('upvotes')
         support = request.args.get('support')
         topic = request.args.get('topic')
+        print "topic, collabCount, cosineCount, averageViews, averageUpvote, support"
+        print topic, collabCount, cosineCount, averageViews, averageUpvote, support
         return jsonify(filter_reco(topic, collabCount, cosineCount, averageViews, averageUpvote, support))
 
 
@@ -162,7 +174,6 @@ def get_description_():
             })
 
 
-
 if __name__ == '__main__':
     if not app.debug:
         import logging
@@ -171,4 +182,7 @@ if __name__ == '__main__':
         file_handler = RotatingFileHandler(filename="app_debug.log")
         file_handler.setLevel(logging.ERROR)
         app.logger.addHandler(file_handler)
+    qsp = map(lambda x: x, conn[DB][PERCENTILES].find({"Id": "Avg_Question_score"}))
+    avp = map(lambda x: x, conn[DB][PERCENTILES].find({"Id": "Avg_View_count"}))
+    ssp = map(lambda x: x, conn[DB][PERCENTILES].find({"Id": "Support_Percentile"}))
     app.run(host='0.0.0.0', port=3000)
